6b0bb98154014a457c15427bb7aa51b5d9c36c7e
niko-suave09 <60201821+niko-suave09@users.noreply.github.com>
2021-02-21 17:56:07 -0500

    Update register.php

--- register.php
+++ register.php
@@ -155,5 +155,9 @@ echo "<script>alert('Data not inserted');</script>";
                 </form>
             </div>
         </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
