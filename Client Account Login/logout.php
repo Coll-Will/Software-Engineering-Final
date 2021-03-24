438ef33189acb60f1a6fb2119d25c9e1c8ff86a6
Coll-Will <77999910+Coll-Will@users.noreply.github.com>
2021-03-23 17:03:27 -0700

    Delete Client Account Login directory

--- Client Account Login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start(); //starts session
-	session_unset(); //clears values with the $_SESSION[] variable
-	if(session_destroy()) 
-	{
-		header("location:login.php");
-		exit;
-	}
-?>
--- Client Account Login/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start(); //starts session
-
-	if(isset($_SESSION["userid"]) && $_SESSION["userid"]==true) {
-	    header("location: welcome.php");
-	    exit;
-	}
-?>
